064b99d34bff009e1a2ee8c89b8516e027664355
Mjjbj67 <165726694+Mjjbj67@users.noreply.github.com>
2024-06-24 03:46:47 +0100

    Update help.js

--- script/help.js
+++ script/help.js
@@ -1,137 +1,125 @@
-const randomQuotes = [
-	"Octopuses have three hearts: two pump blood to the gills, and one pumps it to the rest of the body.",
-		"Honey never spoils; archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old.",
-		"The world's oldest known recipe is for beer.",
-		"Bananas are berries, but strawberries are not.",
-		"Cows have best friends and can become stressed when they are separated.",
-		"The shortest war in history was between Britain and Zanzibar on August 27, 1896; Zanzibar surrendered after 38 minutes.",
-		"The average person walks the equivalent of three times around the world in a lifetime.",
-		"Polar bears are left-handed.",
-		"The unicorn is Scotland's national animal.",
-		"A group of flamingos is called a 'flamboyance'.",
-		"There are more possible iterations of a game of chess than there are atoms in the known universe.",
-		"The smell of freshly-cut grass is actually a plant distress call.",
-		"A day on Venus is longer than its year.",
-		"Honeybees can recognize human faces.",
-		"Wombat poop is cube-shaped.",
-		"The first oranges weren't orange.",
-		"The longest time between two twins being born is 87 days.",
-		"A bolt of lightning is six times hotter than the sun.",
-		"A baby puffin is called a puffling.",
-		"A jiffy is an actual unit of time: 1/100th of a second.",
-		"The word 'nerd' was first coined by Dr. Seuss in 'If I Ran the Zoo'.",
-		"There's a species of jellyfish that is biologically immortal.",
-		"The Eiffel Tower can be 6 inches taller during the summer due to the expansion of the iron.",
-		"The Earth is not a perfect sphere; it's slightly flattened at the poles and bulging at the equator.",
-		"A hummingbird weighs less than a penny.",
-		"Koalas have fingerprints that are nearly identical to humans'.",
-		"There's a town in Norway where the sun doesn't rise for several weeks in the winter, and it doesn't set for several weeks in the summer.",
-		"A group of owls is called a parliament.",
-		"The fingerprints of a koala are so indistinguishable from humans' that they have on occasion been confused at a crime scene.",
-		"The Hawaiian alphabet has only 13 letters.",
-		"The average person spends six months of their life waiting for red lights to turn green.",
-		"A newborn kangaroo is about 1 inch long.",
-		"The oldest known living tree is over 5,000 years old.",
-		"Coca-Cola would be green if coloring wasn't added to it.",
-		"A day on Mars is about 24.6 hours long.",
-		"The Great Wall of China is not visible from space without aid.",
-		"A group of crows is called a murder.",
-		"There's a place in France where you can witness an optical illusion that makes you appear to grow and shrink as you walk down a hill.",
-		"The world's largest desert is Antarctica, not the Sahara.",
-		"A blue whale's heart is so big that a human could swim through its arteries.",
-		"The longest word in the English language without a vowel is 'rhythms'.",
-		"Polar bears' fur is not white; it's actually transparent.",
-		"The electric chair was invented by a dentist.",
-		"An ostrich's eye is bigger than its brain.",
-		"Wombat poop is cube-shaped.",
-"Even a small amount of alcohol poured on a scorpion will drive it crazy and sting itself to death.",
-" The crocodile can't stick its tongue out.","The oldest known animal in the world is a 405-year-old male, discovered in 2007.","Sharks, like other fish, have their reproductive organs located in the ribcage.","The eyes of the octopus have no blind spots. On average, the brain of an octopus has 300 million neurons. When under extreme stress, some octopuses even eat their trunks.","An elephant's brain weighs about 6,000g, while a cat's brain weighs only approximately 30g.","Cats and dogs have the ability to hear ultrasound.","Sheep can survive up to 2 weeks in a state of being buried in snow.","The smartest pig in the world is owned by a math teacher in Madison, Wisconsin (USA). It has the ability to memorize worksheets multiplying to 12.","Statistics show that each rattlesnake's mating lasts up to ... more than 22 hours", "Studies have found that flies are deaf.","In a lack of water, kangaroos can endure longer than camels.","","Dogs have 4 toes on their hind legs and 5 toes on each of their front paws.","The average flight speed of honey bees is 24km/h. They never sleep.","Cockroaches can live up to 9 days after having their heads cut off.","If you leave a goldfish in the dark for a long time, it will eventually turn white.","The flying record for a chicken is 13 seconds.","The mosquito that causes the most deaths to humans worldwide is the mosquito.","TThe quack of a duck doesn't resonate, and no one knows why.","Sea pond has no brain. They are also among the few animals that can turn their stomachs inside out.","Termites are active 24 hours a day and they do not sleep. Studies have also found that termites gnaw wood twice as fast when listening to heavy rock music.","Baby giraffes usually fall from a height of 1.8 meters when they are born.", "A tiger not only has a striped coat, but their skin is also streaked with stripes.."," Vultures fly without flapping their wings.","Turkeys can reproduce without mating.","Penguins are the only birds that can swim, but not fly. Nor have any penguins been found in the Arctic."," The venom of the king cobra is so toxic that just one gram can kill 150 people.","The venom of a small scorpion is much more dangerous than the venom of a large scorpion.","The length of an oyster's penis can be so 'monstrous' that it is 20 times its body size!","Rat's heart beats 650 times per minute.","The flea can jump 350 times its body length. If it also possessed that ability, a human would be able to jump the length of a football field once.","The faster the kangaroo jumps, the less energy it consumes.","Elephants are among the few mammals that can't jump! It was also discovered that elephants still stand after death.","Spiders have transparent blood."," Snails breathe with their feet.","Some lions mate more than 50 times a day.","Chuột reproduce so quickly that in just 18 months, from just 2 mice, the mother can give birth to 1 million heirs.","Hedgehog floats on water.","Alex is the world's first African gray parrot to question its own existence: What color am I?.","The reason why flamingos are pink-red in color is because they can absorb pigments from the shells of shrimp and shrimp that they eat every day."," Owls and pigeons can memorize human faces", "Cows are more dangerous than sharks","The single pair of wings on the back and the rear stabilizer help the flies to fly continuously, but their lifespan is not more than 14 days.","With a pair of endlessly long legs that can be up to 1.5 m high and weigh 20-25 kg, the ostrich can run faster than a horse. In addition, male ostriches can roar like a lion.","Kangaroos use their tails for balance, so if you lift a Kangaroo's tail off the ground, it won't be able to jump and stand.","Tigers not only have stripes on their backs but also printed on their skin. Each individual tiger is born with its own unique stripe.","If you are being attacked by a crocodile, do not try to get rid of their sharp teeth by pushing them away. Just poke the crocodile in the eye, that's their weakness.","Fleas can jump up to 200 times their height. This is equivalent to a man jumping on the Empire State Building in New York.","A cat has up to 32 muscles in the ear. That makes them have superior hearing ability","Koalas have a taste that does not change throughout life, they eat almost nothing but .. leaves of the eucalyptus tree.","The beaver's teeth do not stop growing throughout its life. If you do not want the teeth to be too long and difficult to control, the beaver must eat hard foods to wear them down.","Animals living in coastal cliffs or estuaries have extremely weird abilities. Oysters can change sex to match the mating method.","Butterflies have eyes with thousands of lenses similar to those on cameras, but they can only see red, green, and yellow..","Don't try this at home, the truth is that if a snail loses an eye, it can recover.","Giraffes do not have vocal cords like other animals of the same family, their tongues are blue-black.","Dog nose prints are like human fingerprints and can be used to identify different dogs."
-];
+const fs = require("fs-extra");
+const axios = require("axios");
+const path = require("path");
+const { getPrefix } = global.utils;
+const { commands, aliases } = global.GoatBot;
+const doNotDelete = "[  ✰BOT 😈 | ☆]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+module.exports = {
+	config: {
+		name: "help",
+		version: "1.17",
+		author: "NTKhang", // original author Kshitiz 
+		countDown: 5,
+		role: 0,
+		shortDescription: {
+			en: "View command usage and list all commands directly",
+		},
+		longDescription: {
+			en: "View command usage and list all commands directly",
+		},
+		category: "cmd-list",
+		guide: {
+			en: "{pn} / help cmdName ",
+		},
+		priority: 1,
+	},
 
- const randomQuote = randomQuotes[Math.floor(Math.random() * randomQuotes.length)];
+	onStart: async function ({ message, args, event, threadsData, role }) {
+		const { threadID } = event;
+		const threadData = await threadsData.get(threadID);
+		const prefix = getPrefix(threadID);
 
+		if (args.length === 0) {
+			const categories = {};
+			let msg = "";
 
-module.exports.config = {
-	name: 'help',
-	version: '1.0.0',
-	role: 0,
-	hasPrefix: true,
-	aliases: ['help'],
-	description: "Beginner's guide",
-	usage: "Help [page] or [command]",
-	credits: 'Develeoper',
-};
+			msg += `\n\n  ╞═════𖠁𝘾𝙢𝙙𝙨_𝙇𝙞𝙨𝙩═════╡ `; // replace with your name 
 
-module.exports.run = async function ({
-	api,
-	event,
-	enableCommands,
-	args,
-	Utils,
-	prefix
-}) {
-	const input = args.join(' ');
-	try {
-		const eventCommands = enableCommands[1].handleEvent;
-		const commands = enableCommands[0].commands;
-		if (!input) {
-			const pages = 999;
-			let page = 1;
-			let start = (page - 1) * pages;
-			let end = start + pages;
-			let helpMessage = `𝗛𝗔𝗖𝗞𝗔𝗜𝗔[🌐]⮕𝗕𝗢𝗧 ☪\n\n`;
-			for (let i = start; i < Math.min(end, commands.length); i++) {
-				helpMessage += `\n ☪${i + 1}  ✦ ${prefix}${commands[i]}\n`;
+			for (const [name, value] of commands) {
+				if (value.config.role > 1 && role < value.config.role) continue;
+
+				const category = value.config.category || "Uncategorized";
+				categories[category] = categories[category] || { commands: [] };
+				categories[category].commands.push(name);
 			}
-			helpMessage += '\n𝗘𝗩𝗘𝗡𝗗 𝗖𝗠𝗗 𝗟𝗜𝗦𝗧 [📑]\n━━━━━━━━━━━\n';
-			eventCommands.forEach((eventCommand, index) => {
-				helpMessage += ` ☪ ${index + 1} ✦  ${prefix}${eventCommand}\n`;
+
+			Object.keys(categories).forEach((category) => {
+				if (category !== "info") {
+					msg += `\n╭━━༺${category.toUpperCase()}༻━━𒁍 `;
+
+
+					const names = categories[category].commands.sort();
+					for (let i = 0; i < names.length; i += 3) {
+						const cmds = names.slice(i, i + 3).map((item) => ` ❀${item}`);
+						msg += `\n│${cmds.join(" ".repeat(Math.max(1, 10 - cmds.join("").length)))}`;
+					}
+
+					msg += `\n╰───────────✨`;
+				}
 			});
-			helpMessage += `\n\n  𝗣𝗚𝗔𝗘 [💬] ${page}/${Math.ceil(commands.length / pages)} \n━━━━━━━━━━━\n 𝗛𝗖𝗞𝗔𝗜 [🌐] ⮕𝗕𝗢𝗧 ✦ 𝗖𝗥𝗘𝗔𝗧𝗘 𝗕𝗬 : 𝗣𝗛𝗔𝗥𝗢𝗨𝗞 ✦ 𝗙𝗕 𝗟𝗜𝗡𝗞 :https://www.facebook.com/more.sidibe.1?mibextid=JRoKGi \n━━━━━━━━━━━\n 𝗔𝗨𝗧𝗢𝗕𝗢𝗧 ⮕𝗟𝗜𝗡𝗞✦:https://hackay.onrender.com '.`;
-			api.sendMessage(helpMessage, event.threadID, event.messageID);
-		} else if (!isNaN(input)) {
-			const page = parseInt(input);
-			const pages = 100;
-			let start = (page - 1) * pages;
-			let end = start + pages;
-			let helpMessage = `𝗛𝗔𝗖𝗞𝗔𝗜 [🌐]⮕ 𝗕𝗢𝗧✦:\n\n`;
-			for (let i = start; i < Math.min(end, commands.length); i++) {
-				helpMessage += `\t${i + 1}. ☪ ${prefix}${commands[i]} ✦\n`;
-			}
-			helpMessage += '\n 𝗕𝗢𝗧 [📑] :\n\n';
-			eventCommands.forEach((eventCommand, index) => {
-				helpMessage += `\t${index + 1}. ☪ ${prefix}${eventCommand} ✦\n`;
+
+			const totalCommands = commands.size;
+			msg += `\n☆BOT 𝐡𝐚𝐬  ${totalCommands} 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐬 ✔\n`;
+			msg += `${prefix}𝐡𝐞𝐥𝐩 𝐭𝐨 𝐥𝐨𝐨𝐤 𝐜𝐦𝐝𝐬\n`;
+          msg += `𝐀𝐧𝐲 𝐩𝐫𝐨𝐛𝐥𝐞𝐦 𝐫𝐞𝐥𝐚𝐭𝐞𝐝 𝐭𝐨 𝐛𝐨𝐭 𝐭𝐡𝐞𝐧 𝐮𝐬𝐞 ${prefix}𝐜𝐚𝐥𝐥𝐚𝐝\n`;
+			msg += `\n\n`; // its not decoy so change it if you want
+          msg += `❦ 𝐌𝐄𝐑𝐂𝐈☆𝐁𝐄𝐀𝐔𝐂𝐎𝐔𝐏❦
+
+
+
+`;
+
+msg += ` `;
+			await message.reply({
+				body: msg,
 			});
-			helpMessage += `\nPage ${page} of ${Math.ceil(commands.length / pages)}`;
-			api.sendMessage(helpMessage, event.threadID, event.messageID);
 		} else {
-			const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input?.toLowerCase()))?.[1];
-			if (command) {
-				const {
-					name,
-					version,
-					role,
-					aliases = [],
-					description,
-					usage,
-					credits,
-					cooldown,
-					hasPrefix
-				} = command;
-				const roleMessage = role !== undefined ? (role === 0 ? '➛ Permission: user' : (role === 1 ? '➛ Permission: admin' : (role === 2 ? '➛ Permission: thread Admin' : (role === 3 ? '➛ Permission: super Admin' : '')))) : '';
-				const aliasesMessage = aliases.length ? `➛ Aliases: ${aliases.join(', ')}\n` : '';
-				const descriptionMessage = description ? `Description: ${description}\n` : '';
-				const usageMessage = usage ? `➛ Usage: ${usage}\n` : '';
-				const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
-				const versionMessage = version ? `➛ Version: ${version}\n` : '';
-				const cooldownMessage = cooldown ? `➛ Cooldown: ${cooldown} second(s)\n` : '';
-				const message = ` 「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
-				api.sendMessage(message, event.threadID, event.messageID);
+			const commandName = args[0].toLowerCase();
+			const command = commands.get(commandName) || commands.get(aliases.get(commandName));
+
+			if (!command) {
+				await message.reply(`Command "${commandName}" not found.`);
 			} else {
-				api.sendMessage('Command not found.', event.threadID, event.messageID);
+				const configCommand = command.config;
+				const roleText = roleTextToString(configCommand.role);
+				const author = configCommand.author || "Unknown";
+             const category = configCommand.category
+
+				const longDescription = configCommand.longDescription ? configCommand.longDescription.en || "No description" : "No description";
+
+				const guideBody = configCommand.guide?.en || "No guide available.";
+				const usage = guideBody.replace(/{p}/g, prefix).replace(/{n}/g, configCommand.name);
+
+const response = `✣━☆BOT☆━✤      
+                 🅒🅜🅓☆🅘🅝🅕🅞
+
+❐𝙉𝙖𝙢𝙚 ➢${configCommand.name}
+❐𝙊𝙩𝙝𝙚𝙧𝙉𝙖𝙢𝙚 ➢${configCommand.aliases  ? configCommand.aliases.join(", ") : "Do not have"}
+❐𝘾𝙖𝙩𝙚𝙜𝙤𝙧𝙮  ➢${category}	 
+
+❑𝘾𝙢𝙙_𝙈𝙖𝙠𝙚𝙧 ➢${author}
+
+❒𝙍𝙤𝙡𝙚 ➢${roleText}
+❒𝙏𝙞𝙢𝙚 𝙥𝙚𝙧 𝙘𝙢𝙙 ➢${configCommand.countDown || 1}s
+❒𝘿𝙚𝙨𝙘𝙧𝙞𝙥𝙩𝙞𝙤𝙉 ➢${longDescription}
+❒𝙐𝙨𝙖𝙜𝙚 ➢ ${usage}
+`;
+
+				await message.reply(response);
 			}
 		}
-	} catch (error) {
-		console.log(error);
-	}
+	},
 };
+
+function roleTextToString(roleText) {
+	switch (roleText) {
+		case 0:
+			return "0 (All users)";
+		case 1:
+			return "1 (Group administrators)";
+		case 2:
+			return "2 (Admin bot)";
+		default:
+			return "Unknown role";
+	}
+	}
